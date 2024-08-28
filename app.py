dc7c13471b91b67c224a502e5a445569a6185b16
Suhani Timbadiya <Suhani.Timbadiya@CSSSPL.COM>
2024-08-28 14:46:25 +0530

    warnings add

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from apps import crete_app
 
 app = crete_app()
 #small change #0.0.6
+import warnings
+
+
+warnings.filterwarnings("ignore", message=".*CosmosDB cluster.*")
 
 if __name__ == "__main__":
     app.run(debug=True,port=8000)
