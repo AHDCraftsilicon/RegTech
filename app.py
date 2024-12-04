129d8fd4fbd35bc2ed89477f2de2de2662a4b324
Suhani <suhanitimbadiya@gmail.com>
2024-12-04 14:18:32 +0530

    change to websocket just normal

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
 
-    # socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
-    listener = eventlet.listen(8000)  # Set the listening address and port
-    ssl_listener = eventlet.wrap_ssl(listener, ssl_context=ssl_context)  # Wrap the listener with SSL
-    eventlet.wsgi.server(ssl_listener, app)
+    socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
+    # listener = eventlet.listen(8000)  # Set the listening address and port
+    # ssl_listener = eventlet.wrap_ssl(listener, ssl_context=ssl_context)  # Wrap the listener with SSL
+    # eventlet.wsgi.server(ssl_listener, app)
