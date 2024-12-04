6083a908a2f612548357385a66a073c1d418db91
Suhani <suhanitimbadiya@gmail.com>
2024-12-04 12:52:14 +0530

    socket run transfert to eventlet

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from apps import crete_app ,socketios
 import ssl
+import eventlet
+
 
 app = crete_app()
 
@@ -13,4 +15,5 @@ if __name__ == "__main__":
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
 
-    socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
+    # socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
+    eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('0.0.0.0', 8000)), certfile=cert_path, keyfile=key_path), app)
