79f4eb3fd6315b1e73f177c6461e00024a2a7cdd
Suhani <suhanitimbadiya@gmail.com>
2024-12-04 14:21:10 +0530

    127.0.01 ip add

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
 
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
+    eventlet.wsgi.server(eventlet.listen(('127.0.0.1', 443)), app, ssl_context=ssl_context)
 
-    socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
+    # socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
     # listener = eventlet.listen(8000)  # Set the listening address and port
     # ssl_listener = eventlet.wrap_ssl(listener, ssl_context=ssl_context)  # Wrap the listener with SSL
     # eventlet.wsgi.server(ssl_listener, app)
