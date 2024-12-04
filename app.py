063764b22ec03ad7749dcac9f0c87ce74dbcd897
Suhani <suhanitimbadiya@gmail.com>
2024-12-04 13:05:30 +0530

    eventlet

--- app.py
+++ app.py
@@ -16,4 +16,6 @@ if __name__ == "__main__":
     ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
 
     # socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
-    eventlet.wsgi.server(eventlet.listen(('0.0.0.0', 8000)), app, ssl_context=ssl_context)
+    listener = eventlet.listen(('0.0.0.0', 8000))  # Set the listening address and port
+    ssl_listener = eventlet.wrap_ssl(listener, ssl_context=ssl_context)  # Wrap the listener with SSL
+    eventlet.wsgi.server(ssl_listener, app)
