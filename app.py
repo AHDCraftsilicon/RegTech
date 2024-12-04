f0f099bbb3d3e5d7ff5baf52a713c52553196280
Suhani <suhanitimbadiya@gmail.com>
2024-12-04 13:00:16 +0530

    return change to socket

--- app.py
+++ app.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
     ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
 
-    # socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
-    eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('0.0.0.0', 8000)), certfile=cert_path, keyfile=key_path), app)
+    socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
+    # eventlet.wsgi.server(eventlet.wrap_ssl(eventlet.listen(('0.0.0.0', 8000)), certfile=cert_path, keyfile=key_path), app)
