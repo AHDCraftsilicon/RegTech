78fa469817e508b0a856eedd148ca7b4bd8c85b8
Suhani <suhanitimbadiya@gmail.com>
2024-12-04 14:14:14 +0530

    back to socket

--- app.py
+++ app.py
@@ -9,13 +9,15 @@ app = crete_app()
 if __name__ == "__main__":
     # app.run(debug=True,port=8000)
 
-    cert_path = "/etc/letsencrypt/live/regtech.blubeetle.ai/fullchain.pem"
-    key_path = "/etc/letsencrypt/live/regtech.blubeetle.ai/privkey.pem"
+    socketios.run(app, debug=True, port=8000)
 
-    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
+    # cert_path = "/etc/letsencrypt/live/regtech.blubeetle.ai/fullchain.pem"
+    # key_path = "/etc/letsencrypt/live/regtech.blubeetle.ai/privkey.pem"
 
-    # socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
-    listener = eventlet.listen(('0.0.0.0', 8000))  # Set the listening address and port
-    ssl_listener = eventlet.wrap_ssl(listener, ssl_context=ssl_context)  # Wrap the listener with SSL
-    eventlet.wsgi.server(ssl_listener, app)
+    # ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    # ssl_context.load_cert_chain(certfile=cert_path, keyfile=key_path)
+
+    # # socketios.run(app, debug=True,port=8000, ssl_context=ssl_context)
+    # listener = eventlet.listen(('0.0.0.0', 8000))  # Set the listening address and port
+    # ssl_listener = eventlet.wrap_ssl(listener, ssl_context=ssl_context)  # Wrap the listener with SSL
+    # eventlet.wsgi.server(ssl_listener, app)
