3f56dbd8743b3e6117622320f3a22159a48d19af
Suhani <suhanitimbadiya@gmail.com>
2024-12-19 17:34:04 +0530

    app file changes

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ app = crete_app()
 
 
 if __name__ == "__main__":
-    # app.run(debug=True,port=8000)
-
     socketios.run(app, debug=True, port=8000)
 
     # cert_path = "/etc/letsencrypt/live/regtech.blubeetle.ai/fullchain.pem"
